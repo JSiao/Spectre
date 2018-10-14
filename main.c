cd5ab7ca5aeaf1f2c2fea81969ee4ef3101b6425
JSiao <jecsiao@gmail.com>
2018-10-14 00:47:17 -0700

    Most functionality implemented

--- main.c
+++ main.c
@@ -13,28 +13,28 @@
 int world_map[24][24] =
 {
     {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-    {1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1},
-    {1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1},
-    {1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1},
-    {1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1},
-    {1,0,1,0,1,0,0,0,1,0,1,1,1,1,0,0,0,0,0,1,0,1,0,1},
-    {1,0,1,0,1,0,0,0,1,0,1,0,0,0,0,0,0,0,0,1,0,1,0,1},
-    {1,0,1,0,1,0,0,0,1,0,1,0,0,0,0,0,0,0,0,1,0,1,0,1},
-    {1,0,1,0,1,1,1,0,1,0,1,0,0,0,0,0,0,0,0,1,0,1,0,1},
-    {1,0,1,0,1,0,0,0,1,0,1,0,1,1,0,0,0,0,0,1,0,1,0,1},
+    {1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1},
+    {1,0,1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,0,1},
+    {1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1},
+    {1,0,1,0,1,0,1,1,1,1,1,1,1,1,0,1,0,1,0,1,0,1,1,1},
+    {1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,1},
+    {1,0,1,0,1,0,1,0,1,0,1,1,1,1,0,1,0,1,0,1,0,1,0,1},
+    {1,0,1,0,1,0,1,0,1,0,1,0,0,0,0,1,0,1,0,1,0,1,0,1},
+    {1,0,1,0,0,0,0,0,1,0,1,0,0,0,0,1,0,1,0,1,0,1,0,1},
+    {1,0,1,1,1,1,1,0,1,0,1,0,0,0,0,1,0,0,0,1,0,1,0,1},
+    {1,0,1,0,0,0,0,0,1,0,1,0,1,1,0,0,0,0,0,1,0,1,0,1},
     {1,0,1,0,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1},
-    {1,0,1,0,1,0,0,0,1,0,0,0,0,1,0,0,0,0,0,1,0,1,0,1},
+    {1,0,1,0,1,0,0,0,1,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1},
     {1,0,1,0,1,1,1,0,1,0,1,0,1,1,0,0,0,0,0,1,0,1,0,1},
-    {1,0,1,0,1,0,0,0,1,0,0,0,0,1,0,0,0,0,0,1,0,1,0,1},
+    {1,0,1,0,1,0,0,0,1,0,0,0,0,1,0,1,1,1,0,1,0,1,0,1},
     {1,0,1,0,1,0,1,1,1,0,0,1,0,1,0,0,0,0,0,1,0,1,0,1},
-    {1,0,1,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,1,0,1},
-    {1,0,1,0,1,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,0,1,0,1},
-    {1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1},
-    {1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1},
+    {1,0,1,0,1,0,0,0,0,0,0,1,0,0,0,1,0,1,0,1,0,0,0,1},
+    {1,0,1,0,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,1,0,1,1,1},
+    {1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,1},
+    {1,0,1,0,1,0,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,1,0,1},
     {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1},
-    {1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1},
-    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1},
+    {1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
     {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
 
@@ -119,19 +119,19 @@ short pallette[10];
 double pixel_buffer[320];
 
 //------------------------------------------------------------------
+
 static struct player
 {
     double x,y;
     double dir_x, dir_y;
-} player = {11.5, 11.5, -1, 0};
+} player;
 
 static struct game
 {
     double time_0, time_1;
     char running;
-    char debug;
-    char collide;
-} game_data = {0, 0, 1, 0, 0};
+    int score;
+} game_data;
 
 struct video
 {
@@ -145,9 +145,12 @@ struct sprite
     double x,y;
     char visible;
     char type;
+    char rendered;
+    char direction;
     char (*sprite)[SPRITE_SIZE][SPRITE_SIZE];
-} rune_sprite = {7.5, 11.5, 1, 0, &rune}, spectre_sprite = {1.5, 1.5, 1, 1, &spectre};
+} sprite_list[4];
 
+int sprite_stack[4];
 
 //------------------------------------------------------------------
 
@@ -163,6 +166,10 @@ void  video_wall_line(SDL_Surface *, int, int, int, int, int, int);
 void  video_sprite_line(SDL_Surface *, int, int, int, int, int, char (*)[24][24], int);
 short video_24_to_16(unsigned char, unsigned char, unsigned char);
 
+double distances(struct sprite *, struct player *);
+char  sort(struct sprite *, struct player *, int *);
+char  move(struct sprite *, struct player *);
+char  redirect(struct sprite *, struct player *);
 char  collide(struct player*, struct sprite *);
 
 void  debug_render(struct video *, nSDL_Font *);
@@ -173,14 +180,32 @@ int main(int argc, char **argv)
 {
     if (argc) argc = 0;
     if (argv) argv = NULL;
-    game_init();
+    char flag = 1;
     video_init(&video);
-    SDL_FillRect(video.screen, NULL, SDL_MapRGB(video.screen->format, 0, 0, 0));
-    while(game_data.running)
+    while (flag)
     {
-        key_process(&game_data, &player, &video);
-        video_process(&video, &player);
-        sleep(10);
+        game_init();
+        SDL_FillRect(video.screen, NULL, SDL_MapRGB(video.screen->format, 0, 0, 0));
+        while(game_data.running)
+        {
+            key_process(&game_data, &player, &video);
+            video_process(&video, &player);
+            sleep(1);
+        }
+        nSDL_DrawString(video.screen, video.font, 100, 100, "Game Over. Retry (Y/N):");
+        SDL_Flip(video.screen);
+        while (1)
+        {
+            if (isKeyPressed(KEY_NSPIRE_Y))
+            {
+                break;
+            }
+            if (isKeyPressed(KEY_NSPIRE_N))
+            {
+                flag = 0;
+                break;
+            }
+        }
     }
     SDL_Quit();
     return 0;
@@ -188,15 +213,55 @@ int main(int argc, char **argv)
 
 void game_init(void)
 {
-    pallette[0] = video_24_to_16(  70,  95, 140); //Blue 1
-    pallette[1] = video_24_to_16(  25,  55,  98); //Blue 2
-    pallette[2] = video_24_to_16(  10,  28,  68); //Blue 3
-    pallette[3] = video_24_to_16(  35,  49,  70); //Blue 4
-    pallette[4] = video_24_to_16(  13,  27,  49); //Blue 5
-    pallette[5] = video_24_to_16(   5,  14,  29); //Blue 6
-    pallette[6] = video_24_to_16(   0, 127,   0); //Green
-    pallette[7] = video_24_to_16(   0,   0,   0); //Black
-    pallette[8] = video_24_to_16( 255, 255, 255); //White
+    sprite_list[0].x         = 12.5;
+    sprite_list[0].y         = 11.5;
+    sprite_list[0].visible   = 1;
+    sprite_list[0].type      = 0;
+    sprite_list[0].rendered  = 0;
+    sprite_list[0].direction = 0;
+    sprite_list[0].sprite    = &rune;
+
+    sprite_list[1].x         = 1.5;
+    sprite_list[1].y         = 1.5;
+    sprite_list[1].visible   = 1;
+    sprite_list[1].type      = 1;
+    sprite_list[1].rendered  = 0;
+    sprite_list[1].direction = 1;
+    sprite_list[1].sprite    = &spectre;
+
+    sprite_list[2].x         = 22.5;
+    sprite_list[2].y         = 1.5;
+    sprite_list[2].visible   = 1;
+    sprite_list[2].type      = 1;
+    sprite_list[2].rendered  = 0;
+    sprite_list[2].direction = 1;
+    sprite_list[2].sprite    = &spectre;
+
+    sprite_list[3].x         = 5.5;
+    sprite_list[3].y         = 22.5;
+    sprite_list[3].visible   = 1;
+    sprite_list[3].type      = 1;
+    sprite_list[3].rendered  = 0;
+    sprite_list[3].direction = 1;
+    sprite_list[3].sprite    = &spectre;
+
+    sprite_stack[0] = 0;
+    sprite_stack[1] = 1;
+    sprite_stack[2] = 2;
+    sprite_stack[3] = 3;
+
+    video.plane_x = 0.0f;
+    video.plane_y = 0.6666f;
+
+    game_data.time_0 = 0;
+    game_data.time_1 = 1;
+    game_data.running = 1;
+    game_data.score = 0;
+    player.x = 11.5;
+    player.y = 11.5;
+    player.dir_x = -1;
+    player.dir_y = 0;
+
 }
 
 void key_process(struct game *data, struct player *player, struct video *video)
@@ -206,6 +271,28 @@ void key_process(struct game *data, struct player *player, struct video *video)
     if (isKeyPressed(KEY_NSPIRE_ESC))
     {
         data->running = 0;
+    }
+    if (isKeyPressed(KEY_NSPIRE_SHIFT) || isKeyPressed(KEY_NSPIRE_7))
+    {
+        if (!world_map[(int)(player->x - player->dir_y * move)][(int)(player->y)])
+        {
+            player->x -= player->dir_y * move;
+        }
+        if (!world_map[(int)(player->x)][(int)(player->y + player->dir_x * move)])
+        {
+            player->y += player->dir_x * move;
+        }
+    }
+    if (isKeyPressed(KEY_NSPIRE_VAR) || isKeyPressed(KEY_NSPIRE_9))
+    {
+        if (!world_map[(int)(player->x + player->dir_y * move)][(int)(player->y)])
+        {
+            player->x += player->dir_y * move;
+        }
+        if (!world_map[(int)(player->x)][(int)(player->y - player->dir_x * move)])
+        {
+            player->y -= player->dir_x * move;
+        }
     }
     if (isKeyPressed(KEY_NSPIRE_UP) || isKeyPressed(KEY_NSPIRE_8))
     {
@@ -257,18 +344,47 @@ void video_init(struct video * video_ptr)
 {
     SDL_Init(SDL_INIT_VIDEO);
     video_ptr->screen = SDL_SetVideoMode(320, 240, has_colors ? 16 : 8, SDL_SWSURFACE);
-    video_ptr->plane_x = 0.0f;
-    video_ptr->plane_y = 0.6666f;
     video_ptr->font = nSDL_LoadFont(NSDL_FONT_TINYTYPE, 255, 255, 255);
+
+    pallette[0] = video_24_to_16(  70,  95, 140); //Blue 1
+    pallette[1] = video_24_to_16(  25,  55,  98); //Blue 2
+    pallette[2] = video_24_to_16(  10,  28,  68); //Blue 3
+    pallette[3] = video_24_to_16(  35,  49,  70); //Blue 4
+    pallette[4] = video_24_to_16(  13,  27,  49); //Blue 5
+    pallette[5] = video_24_to_16(   5,  14,  29); //Blue 6
+    pallette[6] = video_24_to_16(   0, 127,   0); //Green
+    pallette[7] = video_24_to_16(   0,   0,   0); //Black
+    pallette[8] = video_24_to_16( 255, 255, 255); //White
 }
 
 void video_process(struct video *video_ptr, struct player *player_ptr)
 {
     SDL_FillRect(video.screen, NULL, SDL_MapRGB(video.screen->format, 10, 10, 10));
     video_render_wall(video_ptr, player_ptr);
-    video_render_sprites(video_ptr, player_ptr, &rune_sprite);
-    game_data.debug = video_render_sprites(video_ptr, player_ptr, &spectre_sprite);
-    game_data.collide = collide(player_ptr, &rune_sprite);
+    sort(sprite_list, player_ptr, sprite_stack);
+    for (int y = 0; y < 4; y++)
+    {
+        sprite_list[sprite_stack[y]].rendered=video_render_sprites(video_ptr, player_ptr, sprite_list+sprite_stack[y]);
+        if (collide(player_ptr, sprite_list+sprite_stack[y]))
+        {
+            if (sprite_stack[y])
+            {
+                game_data.running = 0;
+            }
+            else
+            {
+                do
+                {
+                    sprite_list[0].x = rand() % 24;
+                    sprite_list[0].y = rand() % 24;
+                } while (world_map[(int)sprite_list[0].x][(int)sprite_list[0].y]);
+                sprite_list[0].x = sprite_list[0].x + 0.5f;
+                sprite_list[0].y = sprite_list[0].y + 0.5f;
+                game_data.score ++;
+            }
+        }
+        move(sprite_list+sprite_stack[y], player_ptr);
+    }
     debug_render(&video, video_ptr->font);
     SDL_Flip(video_ptr->screen);
 }
@@ -401,7 +517,7 @@ char video_render_sprites(struct video *video_ptr, struct player *player_ptr, st
 
 void video_sprite_line(SDL_Surface *surface, int x, int start, int end, int tex_x, int type, char (*sprite)[SPRITE_SIZE][SPRITE_SIZE], int line_height)
 {
-    if (type) type = 0;
+    type = type;
     for (int y = start; y < end; y++)
     {
         int d = y * 256 - 240 * 128 + line_height * 128;
@@ -495,22 +611,76 @@ short video_24_to_16(unsigned char red, unsigned char green, unsigned char blue)
     return (r5<<11) | (g6<<5) | b5;
 }
 
-void debug_render(struct video *video, nSDL_Font *font)
+char move(struct sprite *sprite, struct player *player)
 {
-    static int y = 0;
-    char debug[32];
-    snprintf(debug, 32, "See this: %d", y);
-    nSDL_DrawString(video->screen, font, 10, 10, "Justin was Here. \x1");
-    nSDL_DrawString(video->screen, font, 10, 0, debug);
-    snprintf(debug, 32, "Player x: %f", player.x);
-    nSDL_DrawString(video->screen, font, 10, 20, debug);
-    snprintf(debug, 32, "Player y: %f", player.y);
-    nSDL_DrawString(video->screen, font, 10, 30, debug);
-    snprintf(debug, 32, "spectre: %s", ((game_data.debug)?"TRUE":"FALSE"));
-    nSDL_DrawString(video->screen, font, 10, 40, debug);
-    snprintf(debug, 32, "spectre: %s", ((game_data.collide)?"TRUE":"FALSE"));
-    nSDL_DrawString(video->screen, font, 10, 50, debug);
-    y++;
+    if (sprite->type)
+    {
+        if (!sprite->rendered)
+        {
+            double x = 0;
+            double y = 0;
+            double dx = 0;
+            double dy = 0;
+            switch(sprite->direction)
+            {
+            case 1:
+                y = -0.5;
+                dy = -0.25;
+                break;
+            case 2:
+                x = 0.5;
+                dx = 0.25;
+                break;
+            case 3:
+                y = 0.5;
+                dy = 0.25;
+                break;
+            case 4:
+                x = -0.5;
+                dx = -0.25;
+                break;
+            default:
+                return 0;
+            }
+            if (world_map[(int)(sprite->x + x)][(int)(sprite->y + y)])
+            {
+                sprite->direction = (sprite->direction + rand()%3) % 4 + 1;
+            }
+            else
+            {
+                sprite->y += dy;
+                sprite->x += dx;
+            }
+        }
+        else
+        {
+            double dirx = player->x - sprite->x;
+            double diry = player->y - sprite->y;
+            if (fabs(dirx) > fabs(diry))
+            {
+                if (dirx < 0)
+                {
+                    sprite->direction = 4;
+                }
+                else
+                {
+                    sprite->direction = 2;
+                }
+            }
+            else
+            {
+                if (diry < 0)
+                {
+                    sprite->direction = 1;
+                }
+                else
+                {
+                    sprite->direction = 3;
+                }
+            }
+        }
+    }
+    return 0;
 }
 
 char collide(struct player* player, struct sprite *sprite)
@@ -523,6 +693,43 @@ char collide(struct player* player, struct sprite *sprite)
         if (distance < 0.0625)
         {
             return 1;
+        }
+    }
+    return 0;
+}
+
+void debug_render(struct video *video, nSDL_Font *font)
+{
+    char debug[32];
+    nSDL_DrawString(video->screen, font, 10, 10, "Justin \"XRONIKOS\" Siao made this");
+    snprintf(debug, 32, "Score: %d", game_data.score);
+    nSDL_DrawString(video->screen, font, 10, 30, debug);
+}
+
+double distances(struct sprite *sprite, struct player *player)
+{
+    double dx = player->x - sprite->x;
+    double dy = player->y - sprite->y;
+    return dx * dx + dy * dy;
+}
+
+char sort(struct sprite *sprite_list, struct player *player, int *stack)
+{
+    float distance[4];
+    distance[0] = distances(sprite_list, player);
+    distance[1] = distances(sprite_list+1, player);
+    distance[2] = distances(sprite_list+2, player);
+    distance[3] = distances(sprite_list+3, player);
+    for (int x = 0; x < 3; x++)
+    {
+        for (int y = 0; y < 3 -x; y++)
+        {
+            if (distance[stack[y]] < distance[stack[y+1]])
+            {
+                stack[y]   ^= stack[y+1];
+                stack[y+1] ^= stack[y];
+                stack[y]   ^= stack[y+1];
+            }
         }
     }
     return 0;
